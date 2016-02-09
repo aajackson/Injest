683d084b6d3fb891cab52c30195b5f8299778ab5
Drew Jackson <aajackson@smu.edu>
2016-02-08 20:55:56 -0800

    Basic movements - AI using pathfinding and Screen space rays - Player using direct movement

---
+++ Injest/Assets/Scripts/AIMovement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class AIMovement : MonoBehaviour
+{
+    public float DirectionScale = 1.0f;
+
+    private NavMeshAgent navMeshAgent;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            float angle = Random.Range(0.0f, 360.0f);
+            Vector3 direction = Quaternion.AngleAxis(angle, Vector3.up) * Vector3.forward;
+            navMeshAgent.destination = transform.position + direction * DirectionScale;
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            // Cast a ray from the camera
+            Ray cameraRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+            if (Physics.Raycast(cameraRay, out hitInfo))
+            {
+                navMeshAgent.destination = hitInfo.point;
+            }
+        }
+    }
+}
---
+++ Injest/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    public float MoveSpeed = 5.0f;
+
+    private Transform cameraTransform;
+
+    private void Start()
+    {
+        cameraTransform = Camera.main.transform;
+    }
+
+    private void FixedUpdate()
+    {
+        // Inputs
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        // Calculate direction to move the character
+        Vector3 cameraForward = Vector3.Scale(cameraTransform.forward, new Vector3(1, 0, 1)).normalized;
+        Vector3 cameraRight = cameraTransform.right;
+        Vector3 playerMove = (v * cameraForward + h * cameraRight);
+        if (playerMove.sqrMagnitude > 1.0f)
+        {
+            playerMove.Normalize();
+        }
+
+        // TODO: Project onto ground plane for slopes
+        transform.position += playerMove * MoveSpeed * Time.fixedDeltaTime;
+
+    }
+}
